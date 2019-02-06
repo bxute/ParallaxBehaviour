537676b6a2be9a34c8ce84849d5076027b0a5c68
Ankit Kumar <ankit.kumar071460@gmail.com>
2019-02-06 23:33:53 +0530

    fixed the flickering issue in sheet behaviour

--- app/src/main/java/com/bxute/custombottomsheet/ParallaxViewBehaviour.java
+++ app/src/main/java/com/bxute/custombottomsheet/ParallaxViewBehaviour.java
@@ -9,7 +9,6 @@ import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.design.widget.CoordinatorLayout;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.util.TypedValue;
 import android.view.View;
 
